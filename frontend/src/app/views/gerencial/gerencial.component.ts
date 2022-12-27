d9af775da83c1276795620a2d195c3d6876c482d
Pedro256 <pedroh93601@gmail.com>
2022-12-27 11:22:32 -0400

    feat: gerar pdf relatorio

--- frontend/src/app/views/gerencial/gerencial.component.ts
+++ frontend/src/app/views/gerencial/gerencial.component.ts
@@ -1,16 +1,14 @@
 import { Component, EventEmitter, OnInit, Output } from '@angular/core';
-import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import { ApiService } from 'src/app/services/api/api.service';
-import Immobile from 'src/view-models/models/immobile';
-import User from 'src/view-models/models/user';
+import * as html2pdf from 'html2pdf.js'
 
 @Component({
   selector: 'app-gerencial-component',
   templateUrl: './gerencial.component.html',
-  styleUrls:['./gerencial.component.css']
+  styleUrls: ['./gerencial.component.css']
 })
 export class GerencialComponent implements OnInit {
-  
+
 
   constructor(
     private apiService: ApiService
@@ -18,7 +16,44 @@ export class GerencialComponent implements OnInit {
   }
   ngOnInit(): void {
   }
-  
+
+
+  downloadPDF() {
+    var areaDownload = document.getElementById('area-download');
+    if(areaDownload){
+      areaDownload.style.visibility = "hidden";
+    }
+    
+
+    var element = document.getElementById('toprint');
+    var opt = {
+      margin: 0.4,
+      filename: 'relatorio.pdf',
+      image: { type: 'jpeg', quality: 0.98 },
+      html2canvas: { 
+        scale: 2,
+        dpi:300,
+        letterRendering:true,
+        useCORS: true
+      },
+      jsPDF: { unit: 'mm', format: 'a4', orientation: 'portrait' },
+      pagebreak:{
+        mode:['css','legacy'],
+        // mode: ['avoid-all', 'css', 'legacy']
+      }
+    };
+
+    // New Promise-based usage:
+    html2pdf().from(element).set(opt).save();
+
+    setTimeout(()=>{
+      if(areaDownload){
+        areaDownload.style.visibility = "initial";
+      }
+    },3000)
+    
+    
+  }
 
 
 
